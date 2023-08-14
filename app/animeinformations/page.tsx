f48bafe242c2fbb5c74d590720380065e4506e57
Daniele Basile <daniele.basile.job@gmail.com>
2023-08-14 19:34:29 +0200

    updateds

--- app/animeinformations/page.tsx
+++ app/animeinformations/page.tsx
@@ -1,16 +1,4 @@
-"use client";
-import { fetchAnime } from "@/utils/FetchFromApi";
-import { useEffect, useState } from "react";
-
 const page = () => {
-	const [animeData, setAnimeData] = useState([]);
-	useEffect(() => {
-		fetchAnime().then((data) => {
-			let animeDataToPass = data.data;
-			setAnimeData(animeDataToPass);
-		});
-	}, []);
-	console.log("animed", animeData);
 	return (
 		<div>
 			<h1>anime informations</h1>
