5ae8aed1190a2326caa875f975a7fe0948ddffae
Daniele Basile <daniele.basile.job@gmail.com>
2023-08-17 11:43:55 +0200

    modified navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import Navbar from "@/components/Navbar";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,11 @@
 "use client";
-import { useEffect, useRef, useState } from "react";
 import {
 	animeMenuTitlesChoices,
 	mangaAndAnimeMenuTitles,
 	mangaMenuTitlesChoices,
 } from "@/constants";
 import Link from "next/link";
+import { useState } from "react";
 
 const Navbar = () => {
 	const [selectedMenus, setSelectedMenus] = useState({
@@ -13,77 +13,69 @@ const Navbar = () => {
 		manga: false,
 		lastUpdates: false,
 	});
-	const [menuPosition, setMenuPosition] = useState({ top: 0, left: 0 });
-
-	const animeMenuRef = useRef(null);
-	const mangaMenuRef = useRef(null);
-	const calculateMenuPosition = (titleRef: any) => {
-		if (titleRef.current) {
-			const { top, left, height } = titleRef.current.getBoundingClientRect();
-			setMenuPosition({ top: top + height, left });
-		}
-	};
-	useEffect(() => {
-		if (selectedMenus.anime) {
-			calculateMenuPosition(animeMenuRef);
-		} else if (selectedMenus.manga) {
-			calculateMenuPosition(mangaMenuRef);
-		}
-	}, [selectedMenus.anime, selectedMenus.manga]);
-
-	const handleTitleClick = (title: string) => {
+	const setRelatedDropDownMenu = (menuTitle: string) => {
 		setSelectedMenus({
-			anime: title === "Anime" ? !selectedMenus.anime : false,
-			manga: title === "Manga" ? !selectedMenus.manga : false,
+			anime: menuTitle === "Anime" ? !selectedMenus.anime : false,
+			manga: menuTitle === "Manga" ? !selectedMenus.manga : false,
 			lastUpdates:
-				title === "Last Updates" ? !selectedMenus.lastUpdates : false,
+				menuTitle === "Last Updates" ? !selectedMenus.lastUpdates : false,
 		});
 	};
-
 	return (
-		<ul className="flex justify-between items-center m-[3rem] font-bold">
-			{mangaAndAnimeMenuTitles.map((animeOrManga) => (
-				<li
-					className="relative"
-					key={animeOrManga.id}
-					onClick={() => handleTitleClick(animeOrManga.mangaAndAnimeMenuTitle)}
-				>
-					{animeOrManga.mangaAndAnimeMenuTitle}
-					{animeOrManga.mangaAndAnimeMenuTitle === "Anime" &&
-						selectedMenus.anime && (
-							<ul
-								className="absolute left-0 mt-6"
-								style={{ top: menuPosition.top, left: menuPosition.left }}
-							>
-								{animeMenuTitlesChoices.map((anime) => (
-									<Link href={anime.url}>
-										<li key={anime.id}>{anime.animeMenuListName}</li>
-									</Link>
-								))}
-							</ul>
-						)}
-					{animeOrManga.mangaAndAnimeMenuTitle === "Manga" &&
-						selectedMenus.manga && (
-							<ul
-								className="absolute left-[38rem] mt-6"
-								style={{ top: menuPosition.top, left: menuPosition.left }}
-							>
-								{mangaMenuTitlesChoices.map((manga) => (
-									<Link href={manga.url}>
-										<li key={manga.id}>{manga.mangaMenuListName}</li>
-									</Link>
-								))}
-							</ul>
-						)}
-					{animeOrManga.mangaAndAnimeMenuTitle === "Last Updates" &&
-						selectedMenus.lastUpdates && (
-							<ul>
-								{/* Render the appropriate list for "Last Updates" here */}
-							</ul>
-						)}
-				</li>
-			))}
-		</ul>
+		<div>
+			{/* className="inline-block relative" */}
+			<div className="relative">
+				{mangaAndAnimeMenuTitles.map((animeMenuTitles) => (
+					<button
+						className="bg-gray-300 text-gray-700 font-semibold py-2 px-4 rounded inline-flex items-center"
+						onClick={() =>
+							setRelatedDropDownMenu(animeMenuTitles.mangaAndAnimeMenuTitle)
+						}
+					>
+						<span className="mr-1" key={animeMenuTitles.id}>
+							{animeMenuTitles.mangaAndAnimeMenuTitle}
+							{animeMenuTitles.mangaAndAnimeMenuTitle === "Anime" &&
+								selectedMenus.anime && (
+									<ul className="absolute text-gray-700 pt-1">
+										{animeMenuTitlesChoices.map((animeC) => (
+											<Link href={animeC.url}>
+												<li
+													key={animeC.id}
+													className="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
+												>
+													{animeC.animeMenuListName}
+												</li>
+											</Link>
+										))}
+									</ul>
+								)}
+							{animeMenuTitles.mangaAndAnimeMenuTitle === "Manga" &&
+								selectedMenus.manga && (
+									<ul className="absolute text-gray-700 pt-1">
+										{mangaMenuTitlesChoices.map((mangaC) => (
+											<Link href={mangaC.url}>
+												<li
+													key={mangaC.id}
+													className="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
+												>
+													{mangaC.mangaMenuListName}
+												</li>
+											</Link>
+										))}
+									</ul>
+								)}
+						</span>
+						<svg
+							className="fill-current h-4 w-4"
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+						>
+							<path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+						</svg>
+					</button>
+				))}
+			</div>
+		</div>
 	);
 };
 
